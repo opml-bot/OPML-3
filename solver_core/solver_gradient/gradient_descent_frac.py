39dabd9dba7caa54c07ea24992cd5339e3532166
bluzer2001 <96445101+bluzer2001@users.noreply.github.com>
2022-03-29 22:46:22 +0300

    Update gradient_descent_frac.py

--- solver_core/solver_gradient/gradient_descent_frac.py
+++ solver_core/solver_gradient/gradient_descent_frac.py
@@ -2,7 +2,7 @@ from typing import Optional, Callable
 import numpy as np
 
 
-class GradientDescentConst:
+class GradientDescentFrac:
 
     def __init__(self,
                  function: Callable,
@@ -78,6 +78,6 @@ if __name__ == '__main__':
     gradient = lambda z, x: np.array([2 * x[0], 2 * x[1]])
     point = [5, 5]
 
-    task = GradientDescentConst(function=func, gradient=gradient, started_point=point)
+    task = GradientDescentFrac(function=func, gradient=gradient, started_point=point)
     answer = task.solve()
     print(answer)
