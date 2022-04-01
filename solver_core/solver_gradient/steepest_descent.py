31612cafda5c47faba285e1b3c57896ed5176e5d
bluzer2001 <bluzer@yandex.ru>
2022-04-01 11:16:08 +0300

    Код переделан под новую функцию рисования

--- solver_core/solver_gradient/steepest_descent.py
+++ solver_core/solver_gradient/steepest_descent.py
@@ -151,8 +151,7 @@ class SteepestGradient:
                  max_iteration: Optional[int] = 500,
                  acc: Optional[float] = 10**-5,
                  print_midterm: Optional[bool] = False,
-                 save_iters_df: Optional[bool] = False,
-                 draw_flag: Optional[bool] = False):
+                 save_iters_df: Optional[bool] = False):
         self.function = function
         self.gradient = gradient
         self.started_point = started_point
@@ -160,19 +159,14 @@ class SteepestGradient:
         self.acc = acc
         self.print_midterm = print_midterm
         self.save_iters_df = save_iters_df
-        self.draw_flag = draw_flag
+        self.history = pd.DataFrame(columns=['x', 'f', 'iteration'])
 
     def solve(self):
         ans = ''
-        if self.draw_flag:
-            if self.started_point.shape[0] != 2:
-                self.draw_flag = False # Может стоит бросать Error?
-            else:
-                draw_data = pd.DataFrame(columns=['x', 'y', 'z', 'iter', 'size'])
-        if self.save_iters_df:
-            iters_df = pd.DataFrame(columns=['f(x)', 'x'])
         alpha = Symbol('alpha')
         new_x = self.started_point
+        if self.save_iters_df:
+            self.history.loc[0] = [np.array(new_x), self.function(new_x), 0]
         for i in range(self.max_iteration):
             x_prev = new_x
             # вывод промежуточных
@@ -183,16 +177,7 @@ class SteepestGradient:
             if self.save_iters_df:
                 if not self.print_midterm:
                     f_x = self.function(new_x)
-                iters_df = iters_df.append({'f(x)': f_x, 'x': new_x}, ignore_index=True)
-            # рисовалка
-            if self.draw_flag:
-                if not self.print_midterm and not self.save_iters_df:
-                    f_x = self.function(new_x)
-                draw_data = draw_data.append({'x': float(new_x[0]),
-                                              'y': float(new_x[1]),
-                                              'z': float(f_x),
-                                              'iter': int(i),
-                                              'size': 3}, ignore_index=True)
+                self.history.loc[i + 1] = [new_x, f_x, i + 1]
 
             gradient_xprev = self.gradient(self.function, x_prev)
             if self.stop_criterion(gradient_xprev):
@@ -206,9 +191,6 @@ class SteepestGradient:
         else:
             code = 1
         ans += f'x: {new_x}\ny: {self.function(new_x)}\ncode: {code}\niters: {i+1}'
-        if self.draw_flag:
-            fig = self.draw(data=draw_data)
-            return ans, fig
         return ans
 
     def one_dim_opt(self, eq):
@@ -227,7 +209,7 @@ class SteepestGradient:
         """
 
         f = lambdify(['alpha'], eq)
-        task = Brandt(f, [0, 1]) # спорный момент: границы поиска минимума. Стоит уточнить.
+        task = Brandt(f, [0, 1])
         x = task.solve()
         return x
 
@@ -251,120 +233,12 @@ class SteepestGradient:
         else:
             return False
 
-    def draw(self, data, ppa=50):
-        if data.shape[0] > 50:
-            step = data.shape[0]//50
-            data = data.iloc[:, ::step]
-
-        razm_x1 = data['x'].max() - data['x'].min()
-        interval_x1 = [data['x'].min() - 0.2*razm_x1, data['x'].max() + 0.2*razm_x1]
-        razm_x2 = data['x'].max() - data['x'].min()
-        interval_x2 = [data['y'].min() - 0.2*razm_x2, data['y'].max() + 0.2*razm_x2]
-
-        df = self.prepare_surface(interval_x1=interval_x1, interval_x2=interval_x2, cnt_points=ppa)
-        min_value = min(filter(np.isfinite, df.values.flatten()))
-        max_value = max(filter(np.isfinite, df.values.flatten()))
-        fig = make_subplots(rows=1, cols=1, specs=[[{'is_3d': True}]])
-        fig.add_trace(go.Surface(x=df.index,
-                                 y=df.columns,
-                                 z=df.values.T,
-                                 opacity=0.5,
-                                 showscale=False,
-                                 colorscale='plasma',
-                                 name='f(x, y)'),
-                      row=1, col=1)
-        fig.add_trace(go.Scatter3d(x=data.x,
-                                   y=data.y,
-                                   z=data.z,
-                                   opacity=0.7,
-                                   mode='lines',
-                                   line={'width': 10}),
-                      row=1, col=1)
-        df_for_cone = self.prepare_arrows(df=data)
-
-        fig.add_trace(go.Cone(x=df_for_cone.x,
-                              y=df_for_cone.y,
-                              z=df_for_cone.z,
-                              u=df_for_cone.u,
-                              v=df_for_cone.v,
-                              w=df_for_cone.w,
-                              showscale=False, sizeref=0.2))
-        fig.update_scenes(xaxis={'range': interval_x1},
-                          yaxis={'range': interval_x2}, row=1, col=1)
-        # fig.add_trace(go.Contour(x=df.index,
-        #                          y=df.columns,
-        #                          z=df.values.T,
-        #                          opacity=0.75,
-        #                          contours={
-        #                              'start': min_value,
-        #                              'end': max_value,
-        #                              'size': (max_value - min_value) // 15,
-        #                          },
-        #                          colorscale='ice',
-        #                          name='f(x, y)'),
-        #               row=1, col=1)
-        return fig
-
-    def prepare_surface(self, interval_x1, interval_x2, cnt_points):
-        """
-
-        Parameters
-        ----------
-        interval_x1
-        interval_x2
-        cnt_points
-
-        Returns
-        -------
-
-        """
-        x_axis = np.linspace(interval_x1[0], interval_x1[1], cnt_points)
-        y_axis = np.linspace(interval_x2[0], interval_x2[1], cnt_points)
-        points = pd.DataFrame(index=x_axis, columns=y_axis)
-        x_axis = list(x_axis)
-        y_axis = list(y_axis)
-
-        for x_i in x_axis:
-            for y_i in y_axis:
-                f = self.function([x_i, y_i])
-                if np.isfinite(f):
-                    points.loc[x_i, y_i] = f
-                else:
-                    points.loc[x_i, y_i] = np.nan
-
-        return points
-
-    def prepare_arrows(self, df):
-        """
-
-        Parameters
-        ----------
-        df
-
-        Returns
-        -------
-
-        """
-
-        xdata = df.x.values.astype(np.float32)
-        ydata = df.y.values.astype(np.float32)
-        zdata = df.z.values.astype(np.float32)
-        df_for_cone = pd.DataFrame({'x': xdata, 'y': ydata, 'z': zdata})
-        veclen = list(np.sqrt(df_for_cone['x'] ** 2 + df_for_cone['y'] ** 2 + df_for_cone['z'] ** 2))
-        df_for_cone['u'] = list((df_for_cone['x'] - df_for_cone['x'].shift()))
-        df_for_cone['v'] = list((df_for_cone['y'] - df_for_cone['y'].shift()))
-        df_for_cone['w'] = list((df_for_cone['z'] - df_for_cone['z'].shift()))
-        print(df_for_cone)
-        return df_for_cone
-
 
 if __name__ == '__main__':
     func = lambda x: (x[0] - 50)**2 + x[1]**2
     gradient = lambda z, x: np.array([2*(x[0]-50), 2*x[1]])
     point = np.array([5, 5])
 
-    task = SteepestGradient(function=func, gradient=gradient, started_point=point, print_midterm=1, max_iteration=5,
-                            draw_flag=True)
+    task = SteepestGradient(function=func, gradient=gradient, started_point=point, print_midterm=1, max_iteration=5)
     answer = task.solve()
-    print(answer[0])
-    answer[1].show()
+    print(answer)
