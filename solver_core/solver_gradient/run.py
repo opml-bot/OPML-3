940ef4e128a7d4603adbdcb69a9d71d40f44f611
bluzer2001 <bluzer@yandex.ru>
2022-04-03 22:53:32 +0300

    Первый тест

---
+++ solver_core/solver_gradient/run.py
@@ -0,0 +1,37 @@
+from IPython.display import display
+from ipywidgets import Dropdown, Textarea, Layout, Button, Text, HTMLMath,IntSlider, FloatText
+
+from solver_core.solver_gradient.gradient_descent_const import GradientDescentConst
+from solver_core.solver_gradient.gradient_descent_frac import GradientDescentFrac
+from solver_core.solver_gradient.steepest_descent import SteepestGradient
+from solver_core.solver_gradient.handlers.input_validation import *
+from messages import *
+
+
+params = {}
+
+def choose_method():
+    text = HTMLMath(
+        value=HELLO_TEXT
+    )
+    dropdown1 = Dropdown(
+        options=[('Градиент с константным шагом', GradientDescentConst),
+                 ('Градиент с дроблением шага', GradientDescentFrac),
+                 ('Наискорейший спуск', SteepestGradient)],
+        value=None,  # Выбор по умолчанию
+        layout=Layout(width='80%', height='80px'),
+        description='Метод:')
+
+    def on_button_clicked(b):
+        global params
+        params['Method'] = b['new']
+        text.layout.display = 'none'
+        dropdown1.layout.display = 'none'
+        set_func()
+
+    dropdown1.observe(on_button_clicked, names='value')
+    display(text)
+    display(dropdown1)
+
+def set_func()
+    pass
