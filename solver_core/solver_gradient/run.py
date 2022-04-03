0916dbf78d06c0f58528c484035abbaf419dc41c
bluzer2001 <bluzer@yandex.ru>
2022-04-03 23:05:35 +0300

    Добавил функцию

--- solver_core/solver_gradient/run.py
+++ solver_core/solver_gradient/run.py
@@ -4,8 +4,13 @@ from ipywidgets import Dropdown, Textarea, Layout, Button, Text, HTMLMath,IntSli
 from .gradient_descent_const import GradientDescentConst
 from .gradient_descent_frac import GradientDescentFrac
 from .steepest_descent import SteepestGradient
-#from solver_core.solver_gradient.handlers.input_validation import *
 from .messages import *
+
+# from gradient_descent_const import GradientDescentConst
+# from gradient_descent_frac import GradientDescentFrac
+# from steepest_descent import SteepestGradient
+# from messages import *
+from solver_core.solver_gradient.handlers.input_validation import *
 
 
 params = {}
@@ -34,4 +39,27 @@ def choose_method():
     display(dropdown1)
 
 def set_func():
-    print('MISWORKING!!!')
+    text = HTMLMath(
+        value=INPUT_FUNCTION
+    )
+    text = Text(value=' ',
+                placeholder='Впиши функцию!',
+                description='Функция:',
+                layout=Layout(width='80%', height='80px'),
+                disabled=False)
+
+    def callback(wdgt):
+        global params
+        try:
+            a = wdgt.value
+            check_expression(a)
+        except:
+            pass
+        params['function'] = wdgt.value
+        text.layout.display = 'none'
+        message_func.layout.display = 'none'
+        set_grad()
+
+    text.on_submit(callback)
+    display(message_func)
+    display(text)
