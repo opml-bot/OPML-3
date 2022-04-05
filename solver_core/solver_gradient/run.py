eea0a813ad88e75a73fcccfaf6c96336914ba106
bluzer2001 <bluzer@yandex.ru>
2022-04-05 10:54:41 +0300

    Убрали принты

--- solver_core/solver_gradient/run.py
+++ solver_core/solver_gradient/run.py
@@ -207,8 +207,6 @@ def set_other():
     def callback(wdgt):
         global params
         try:
-            print(names)
-            print(extra)
             for i in range(len(extra)):
                 params[names[i]] = check_float(extra[i].value)
         except ValueError as err:
@@ -256,20 +254,17 @@ def calculate():
     printer = params['print_midterm']
     dataframe = params['save_iters_df']
     if params['Method'] == GradientDescentConst:
-        print(params)
         alpha_ = params['alpha']
         task = GradientDescentConst(function=func, gradient=grads, started_point=point, alpha=alpha_,
                                     max_iteration=max_iter, acc=accur, print_midterm=printer, save_iters_df=dataframe)
         ans = task.solve()
     if params['Method'] == GradientDescentFrac:
-        print(params)
         alpha_ = params['alpha']
         delta_ = params['delta']
         task = GradientDescentFrac(function=func, gradient=grads, started_point=point, alpha=alpha_, delta=delta_,
                                 max_iteration=max_iter, acc=accur, print_midterm=printer, save_iters_df=dataframe)
         ans = task.solve()
     if params['Method'] == SteepestGradient:
-        print(params)
         task = GradientDescentFrac(function=func, gradient=grads, started_point=point,
                                    max_iteration=max_iter, acc=accur, print_midterm=printer, save_iters_df=dataframe)
         ans = task.solve()
