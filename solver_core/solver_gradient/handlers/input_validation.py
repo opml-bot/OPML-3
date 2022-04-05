b81a11f8d644a0ad5b6ea2b66b28f5bb91071fb1
bluzer2001 <bluzer@yandex.ru>
2022-04-05 11:27:28 +0300

    Исправлен парсер функции

--- solver_core/solver_gradient/handlers/input_validation.py
+++ solver_core/solver_gradient/handlers/input_validation.py
@@ -45,7 +45,7 @@ def check_expression(expression: str) -> tuple:
     function = sympify(expression, {'e': exp(1)}, convert_xor=True)
     if function.free_symbols:
         max_index = max([int(str(i)[1:]) for i in list(function.free_symbols)])
-        variables = [Symbol(f'x{i}') for i in range(1, max_index+1)]
+        variables = [f'x{i}' for i in range(1, max_index+1)]
     else:
         variables = []
     return str(function), variables
