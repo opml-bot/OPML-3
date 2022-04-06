7d5ef7f5df34138236fcd4dd3eb9d367393843c0
nadya <nadya.geraskina@gmail.com>
2022-04-06 23:58:20 +0300

    сделала наконецто табличку

--- solver_core/solver_gradient/test/table.py
+++ solver_core/solver_gradient/test/table.py
@@ -6,12 +6,13 @@ from solver_core.solver_gradient.gradient_descent_const import GradientDescentCo
 from solver_core.solver_gradient.gradient_descent_frac import GradientDescentFrac
 from solver_core.solver_gradient.handlers.preprocessing import *
 from solver_core.solver_gradient.handlers.input_validation import *
+from solver_core.solver_gradient.newtonCG import NewtonCG
 from solver_core.solver_gradient.steepest_descent import SteepestGradient
 from solver_core.solver_gradient.test.funcs_str import funcs_str
 
 
 def prepare_all(func, min_val, point):
-    func = func(0,0)
+    func = func(0, 0)
     s, vars = check_expression(func)
     grad = check_gradients('', vars)
     point = check_point(point)
@@ -25,20 +26,19 @@ def prepare_all(func, min_val, point):
     return [func, grads, point], min_val
 
 
-
 df = pd.DataFrame(
-    columns=['Название', 'Вводные данные', 'Алгоритм1', \
-             'Алгоритм2', 'Алгоритм3', 'Алгоритм4', 'scipy', 'Верный ответ'])
+    columns=['Название', 'Вводные данные', 'Градиентный спуск с const шагом',
+             'Градиентный спуск с дроблёным шагом', 'Наискорейший градиентный спуск',
+             'Ньютона-сопряжённый градиентный спуск', 'scipy', 'Верный ответ'])
 for k, names in enumerate(funcs.keys()):
     xs = list(sp.symbols('x1 x2'))
     point = prepare_point(funcs[names][2])
     a = prepare_all(*funcs_str[names])
-
-    df.loc[k] = [f'{names}', f'x = ({funcs[names][2]})', \
-                 GradientDescentConst(funcs[names][0], prepare_gradient('', xs), point).solve().split('\n')[0],
-                 GradientDescentFrac(funcs[names][0], prepare_gradient('', xs), point).solve().split('\n')[0],
+    df.loc[k] = [f'{names}', f'x = ({funcs[names][2]})',
+                 GradientDescentConst(funcs[names][0], prepare_gradient('', xs), point).solve().split('\n')[1],
+                 GradientDescentFrac(funcs[names][0], prepare_gradient('', xs), point).solve().split('\n')[1],
                  SteepestGradient(a[0][0], a[0][1], a[0][2]).solve().split('\n')[0][
-                 4:-1].split('\n')[0], '', \
+                 4:-1].split('\n')[0], NewtonCG(a[0][0], a[0][2]).solve().split('\n')[0][4:-1].split('\n')[0],
                  f'x = {minimize(funcs[names][0], point).x}', f'{funcs[names][1]}']
 # print(df)
 print(df.to_latex(index=False))
